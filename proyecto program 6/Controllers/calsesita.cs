1a58ee28a1b1a2f5e3042ccf0dd4e3e42bb1571d
fedecampoos <40859084+fedecampoos@users.noreply.github.com>
2023-08-29 17:59:47 -0300

    das

---
+++ proyecto program 6/Controllers/calsesita.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using proyecto_program_6.Models;
+using System.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace proyecto_program_6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class calsesita : ControllerBase
+    {
+        // GET: api/<calsesita>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<calsesita>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<calsesita>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<calsesita>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<calsesita>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet]
+        [Route("obtenerordenes")]
+        public List<OrdenesHistory> obtenerordenes() {
+            string connectionString = "Data Source=NB-CONSUL-117;Initial Catalog=Clase3;Integrated Security=True";
+
+            List<OrdenesHistory> lista = new List<OrdenesHistory>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "select * from ORDERS_HISTORY";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    OrdenesHistory oh = new OrdenesHistory();
+                    oh.TX_NUMBER = int.Parse(reader[0].ToString());
+                    oh.ORDER_DATE = DateTime.Parse(reader[1].ToString());
+
+                    lista.Add(oh);
+
+
+                }
+
+            }
+
+           
+
+                return lista;
+        }
+
+    }
+
+}
---
+++ proyecto program 6/Models/OrdenesHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace proyecto_program_6.Models
+{
+    public class OrdenesHistory
+    {
+        public int TX_NUMBER { get; set; }
+        public DateTime ORDER_DATE { get; set; }
+        public string STATUS { get; set; }
+        public string ACTION { get; set; }
+        public string SYMBOL { get; set; }
+        public int QUANTITY { get; set; }
+        public decimal PRICE { get; set; }
+
+      
+    }
+}
